5080fa9b7216e6e369308727fed37723e2a41ece
Julio Luque <julio.a.luque@gmail.com>
2020-07-02 19:27:54 -0300

    First Commit

---
+++ src/main/java/ar/jluque/java8/App.java
@@ -0,0 +1,13 @@
+package ar.jluque.java8;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
